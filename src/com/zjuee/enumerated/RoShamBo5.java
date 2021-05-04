5caa62fcea69778f4f668ce4ad8d06d76cd8c593
Jackson Croft <lsixun@zju.edu.cn>
2021-05-04 10:53:13 +0800

    2020/5/4 last commit

--- src/com/zjuee/enumerated/RoShamBo5.java
+++ src/com/zjuee/enumerated/RoShamBo5.java
@@ -5,11 +5,10 @@ import static com.zjuee.enumerated.Outcome.*;
 
 public enum RoShamBo5 implements Competitor<RoShamBo5>{
     PAPER, SCISSORS, ROCK;
-    static EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>> table = new EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>>(RoShamBo5.class);
+    static EnumMap<RoShamBo5, EnumMap<RoShamBo5, Outcome>> table = new EnumMap<>(RoShamBo5.class);
     static {
-        for (RoShamBo5 it : RoShamBo5.values()) {
-            table.put(it, new EnumMap<RoShamBo5, Outcome>(RoShamBo5.class));
-        }
+        for (RoShamBo5 it : RoShamBo5.values())
+            table.put(it, new EnumMap<>(RoShamBo5.class));
         initRow(PAPER, DRAW, LOSE, WIN);
         initRow(SCISSORS, WIN, DRAW, LOSE);
         initRow(ROCK, LOSE, WIN, DRAW);
@@ -20,11 +19,9 @@ public enum RoShamBo5 implements Competitor<RoShamBo5>{
         row.put(RoShamBo5.SCISSORS, vSCISSORS);
         row.put(RoShamBo5.ROCK, vROCK);
     }
-    @Override
     public Outcome compete(RoShamBo5 it) {
         return table.get(this).get(it);
     }
-
     public static void main(String[] args) {
         RoShamBo.play(RoShamBo5.class, 20);
     }
