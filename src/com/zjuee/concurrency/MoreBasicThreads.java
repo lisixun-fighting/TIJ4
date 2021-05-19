239acda10c152b7199fa4c74aa0b9863484de87d
Jackson Croft <lsixun@zju.edu.cn>
2021-05-20 00:30:07 +0800

    2020/5/19 second commit

--- src/com/zjuee/concurrency/MoreBasicThreads.java
+++ src/com/zjuee/concurrency/MoreBasicThreads.java
@@ -2,7 +2,7 @@ package com.zjuee.concurrency;
 
 public class MoreBasicThreads {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 10; i++)
             new Thread(new LiftOff()).start();
         System.out.println("Waiting for LiftOff");
     }
--- src/com/zjuee/generics/Fibonacci.java
+++ src/com/zjuee/generics/Fibonacci.java
@@ -4,7 +4,6 @@ import com.zjuee.util.Generator;
 
 public class Fibonacci implements Generator<Integer> {
     private int count = 0;
-    @Override
     public Integer next() {
         return fib(count++);
     }
@@ -12,7 +11,6 @@ public class Fibonacci implements Generator<Integer> {
         if(n < 2) return 1;
         return fib(n-1) + fib(n-2);
     }
-
     public static void main(String[] args) {
         Fibonacci gen = new Fibonacci();
         for (int i = 0; i < 10; i++) {
